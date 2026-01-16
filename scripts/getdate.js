b048d4019928a7e01f1b9d6e0528d679573e5215
mensungqquiwon <jquiwon@byupathway.edu>
2026-01-16 15:30:26 +0000

    enhanced my home page

--- scripts/getdate.js
+++ scripts/getdate.js
@@ -1,2 +1,2 @@
 const year = document.querySelector("#currentYear");
-document.getElementById("lastModified").textContent = new Date().getFullYear();
+document.getElementById("lastModified").textContent = "Last Modified: " + new Date().getFullYear();
